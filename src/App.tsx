ef592369da3626b612eb552aa937be4fb54dc94f
Bradley Justice <bjustice@buxtonco.com>
2023-01-07 23:39:38 -0600

    Cleaned it up again

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useEffect } from "react";
-import { getPlayerBattlePets } from "./api/GetBattlePets";
 import BattlePetDropdownMenu from "./components/BattlePetDropdownMenu";
 
 const App = () => {
-	useEffect(() => {
-		getPlayerBattlePets("Jubbjub", "azjolnerub");
-	}, []);
-
 	return <BattlePetDropdownMenu />;
 };
 
